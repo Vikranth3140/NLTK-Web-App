02117612fd2e59b6b471ced222a3f2586b6c842a
Vikranth3140 <vikranth22570@iiitd.ac.in>
2024-05-16 14:51:49 +0530

    Tried diff buttons for single text box

--- app.py
+++ app.py
@@ -14,22 +14,21 @@ nltk.download('stopwords')
 def home():
     return render_template('index.html')
 
-@app.route('/tokenize', methods=['POST'])
-def tokenize():
-    input_text = request.form['inputText']
-    tokens = word_tokenize(input_text)
-    # Remove stopwords
-    stop_words = set(stopwords.words('english'))
-    filtered_tokens = [word for word in tokens if word.lower() not in stop_words]
-    tokenized_text = ' '.join(filtered_tokens)
-    return render_template('index.html', tokenized_text=tokenized_text)
-
-@app.route('/pos_tag', methods=['POST'])
-def pos_tag():
-    input_sentence = request.form['inputSentence']
-    tokens = word_tokenize(input_sentence)
-    tagged_sentence = pos_tag(tokens)
-    return render_template('index.html', pos_tagged_sentence=tagged_sentence)
+@app.route('/process', methods=['POST'])
+def process():
+    if 'tokenize' in request.form:
+        input_text = request.form['inputText']
+        tokens = word_tokenize(input_text)
+        # Remove stopwords
+        stop_words = set(stopwords.words('english'))
+        filtered_tokens = [word for word in tokens if word.lower() not in stop_words]
+        tokenized_text = ' '.join(filtered_tokens)
+        return render_template('index.html', tokenized_text=tokenized_text)
+    elif 'pos_tag' in request.form:
+        input_sentence = request.form['inputSentence']
+        tokens = word_tokenize(input_sentence)
+        tagged_sentence = pos_tag(tokens)
+        return render_template('index.html', pos_tagged_sentence=tagged_sentence)
 
 if __name__ == '__main__':
     app.run(debug=True)
