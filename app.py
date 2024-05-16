9e28950a65332336507057290f10775122715fc3
Vikranth3140 <vikranth22570@iiitd.ac.in>
2024-05-16 15:01:05 +0530

    Added option for all

--- app.py
+++ app.py
@@ -18,18 +18,26 @@ def home():
 @app.route('/process', methods=['POST'])
 def process():
     input_text = request.form['inputText']
-    
+    option = request.form['option']
+
     # Tokenization and stopwords removal
     tokens = word_tokenize(input_text)
     stop_words = set(stopwords.words('english'))
     filtered_tokens = [word for word in tokens if word.lower() not in stop_words]
     tokenized_text = ' '.join(filtered_tokens)
-    
+
     # POS Tagging
-    tagged_tokens = pos_tag(tokens)
+    tagged_tokens = pos_tag(filtered_tokens)
     pos_tagged_text = ' '.join([f'{token} ({tag})' for token, tag in tagged_tokens])
-    
-    return render_template('index.html', tokenized_text=tokenized_text, pos_tagged_text=pos_tagged_text)
+
+    if option == 'Tokenize':
+        return render_template('index.html', tokenized_text=tokenized_text)
+    elif option == 'PosTag':
+        return render_template('index.html', pos_tagged_text=pos_tagged_text)
+    elif option == 'All':
+        return render_template('index.html', tokenized_text=tokenized_text, pos_tagged_text=pos_tagged_text)
+    else:
+        return render_template('index.html')  # Default render without results
 
 if __name__ == '__main__':
     app.run(debug=True)
