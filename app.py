a6d90cad51835a0803540c2cbdb8bb9a6d6d8109
Vikranth3140 <vikranth22570@iiitd.ac.in>
2024-05-16 14:43:12 +0530

    Added POS Tagging

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 import nltk
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
+from nltk import pos_tag
 
 app = Flask(__name__)
 
@@ -22,6 +23,13 @@ def tokenize():
     filtered_tokens = [word for word in tokens if word.lower() not in stop_words]
     tokenized_text = ' '.join(filtered_tokens)
     return render_template('index.html', tokenized_text=tokenized_text)
+
+@app.route('/pos_tag', methods=['POST'])
+def pos_tag():
+    input_sentence = request.form['inputSentence']
+    tokens = word_tokenize(input_sentence)
+    tagged_sentence = pos_tag(tokens)
+    return render_template('index.html', pos_tagged_sentence=tagged_sentence)
 
 if __name__ == '__main__':
     app.run(debug=True)
